ff855f309ac770c206d773e8e964cde8519018e8
SunTzu423 <boiboi694201738@gmail.com>
2023-12-14 19:07:43 -0500

    Updates

---
+++ precache-manifest.500ae4c3add3e2db407ec7b4e9eccba4.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87cf21784b500dedf6a11a00208bde1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e75a9df0169e6a84386d",
+    "url": "/static/css/main.2330dc1b.chunk.css"
+  },
+  {
+    "revision": "a742e27ddc12fa0b178c",
+    "url": "/static/js/2.533f4702.chunk.js"
+  },
+  {
+    "revision": "85c618a763ccafa2056e3de52ffe41c8",
+    "url": "/static/js/2.533f4702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fde0175fae28c938c343",
+    "url": "/static/js/3.a0c55b76.chunk.js"
+  },
+  {
+    "revision": "e75a9df0169e6a84386d",
+    "url": "/static/js/main.c260b6e4.chunk.js"
+  },
+  {
+    "revision": "b428d29ca066a119b6ee",
+    "url": "/static/js/runtime-main.b16793f7.js"
+  },
+  {
+    "revision": "bae7967f5490f9f3394bdc5ab9ae7010",
+    "url": "/static/media/abtban_01.bae7967f.png"
+  },
+  {
+    "revision": "41c3f75272b6e93c352f666516502ae3",
+    "url": "/static/media/abtban_02.41c3f752.png"
+  },
+  {
+    "revision": "261b31bedf279fb3f8bddd1da49bbced",
+    "url": "/static/media/abtban_03.261b31be.png"
+  },
+  {
+    "revision": "d78a6557ea7e3b032625591772366190",
+    "url": "/static/media/abtban_04.d78a6557.png"
+  },
+  {
+    "revision": "2c91c730f3207611544fcd8a69834513",
+    "url": "/static/media/abtban_05.2c91c730.png"
+  },
+  {
+    "revision": "24acc69736ed4bc45e977c09209d41e4",
+    "url": "/static/media/abtgallery_01.24acc697.png"
+  },
+  {
+    "revision": "7b8fc7714fee87a1ee62fe43a41adf0d",
+    "url": "/static/media/abtgallery_02.7b8fc771.png"
+  },
+  {
+    "revision": "7cc0461bd8b8dfe558f17cb0dfc13ebd",
+    "url": "/static/media/abtgallery_03.7cc0461b.png"
+  },
+  {
+    "revision": "f1347a7a229f75e993dd97cbe8dee585",
+    "url": "/static/media/abtgallery_04.f1347a7a.png"
+  },
+  {
+    "revision": "6551b1d8014a922b333882f8d85f717e",
+    "url": "/static/media/abtgallery_05.6551b1d8.png"
+  },
+  {
+    "revision": "bf789b1b1d3749e679be72e0bb0815cc",
+    "url": "/static/media/abtgallery_06.bf789b1b.png"
+  },
+  {
+    "revision": "ab2d8255b52b611cc9ea2a373ef9913f",
+    "url": "/static/media/abtgallery_07.ab2d8255.png"
+  },
+  {
+    "revision": "c23626c1cff6a995ec330c37192cde04",
+    "url": "/static/media/abtgallery_08.c23626c1.png"
+  },
+  {
+    "revision": "109d97db916c75ff643787a3075ff399",
+    "url": "/static/media/banner.109d97db.png"
+  },
+  {
+    "revision": "deca73331661860766884a1c92802355",
+    "url": "/static/media/banner_bg.deca7333.png"
+  },
+  {
+    "revision": "f35d2c0291bcf6bc7a4e00932d1b8ea0",
+    "url": "/static/media/breadCrumbBg.f35d2c02.png"
+  },
+  {
+    "revision": "88248c1abcbeddb5669c27ba20e9b949",
+    "url": "/static/media/chef-dish.88248c1a.png"
+  },
+  {
+    "revision": "c767a982a1b1e7527919396be5cc40cd",
+    "url": "/static/media/chef_01.c767a982.png"
+  },
+  {
+    "revision": "715b87890d849de7a9e4e91ffd971552",
+    "url": "/static/media/chef_02.715b8789.png"
+  },
+  {
+    "revision": "5d10911097616f1290bf4e44a0ee2c03",
+    "url": "/static/media/chef_03.5d109110.png"
+  },
+  {
+    "revision": "81375a79d6f506bbedd41226e9d17c65",
+    "url": "/static/media/chiken_side.81375a79.png"
+  },
+  {
+    "revision": "9ff5ec323c84178f107a15ed3698eda5",
+    "url": "/static/media/chiken_side2.9ff5ec32.png"
+  },
+  {
+    "revision": "efe849c5b02b195ab7e53f8edccd484c",
+    "url": "/static/media/contact.efe849c5.png"
+  },
+  {
+    "revision": "924281fe25f4193b4d17d4c1322447b6",
+    "url": "/static/media/count_icon.924281fe.svg"
+  },
+  {
+    "revision": "f35d2c0291bcf6bc7a4e00932d1b8ea0",
+    "url": "/static/media/dark_bg.f35d2c02.png"
+  },
+  {
+    "revision": "e71955deb0be4714c1902f9275a842cb",
+    "url": "/static/media/desert1.e71955de.png"
+  },
+  {
+    "revision": "54949a608be6be53185cd3b4d095f4fb",
+    "url": "/static/media/disocunt_bg.54949a60.jpg"
+  },
+  {
+    "revision": "f35d2c0291bcf6bc7a4e00932d1b8ea0",
+    "url": "/static/media/footer_bg.f35d2c02.png"
+  },
+  {
+    "revision": "1d5a4dfe5509b97cc229d1b5cc8fbf6e",
+    "url": "/static/media/gallery_01.1d5a4dfe.png"
+  },
+  {
+    "revision": "1d5a4dfe5509b97cc229d1b5cc8fbf6e",
+    "url": "/static/media/gallery_02.1d5a4dfe.png"
+  },
+  {
+    "revision": "1d5a4dfe5509b97cc229d1b5cc8fbf6e",
+    "url": "/static/media/gallery_03.1d5a4dfe.png"
+  },
+  {
+    "revision": "1d5a4dfe5509b97cc229d1b5cc8fbf6e",
+    "url": "/static/media/gallery_04.1d5a4dfe.png"
+  },
+  {
+    "revision": "1d5a4dfe5509b97cc229d1b5cc8fbf6e",
+    "url": "/static/media/gallery_05.1d5a4dfe.png"
+  },
+  {
+    "revision": "655e1304351331d48636bb0d30db5c57",
+    "url": "/static/media/gallery_06.655e1304.png"
+  },
+  {
+    "revision": "1d5a4dfe5509b97cc229d1b5cc8fbf6e",
+    "url": "/static/media/gallery_07.1d5a4dfe.png"
+  },
+  {
+    "revision": "1d5a4dfe5509b97cc229d1b5cc8fbf6e",
+    "url": "/static/media/gallery_08.1d5a4dfe.png"
+  },
+  {
+    "revision": "50a4ab76e700a83e649be213f820fbbd",
+    "url": "/static/media/icofont.50a4ab76.woff2"
+  },
+  {
+    "revision": "f6ab04aed30a8643bf94fe00f7ff0b59",
+    "url": "/static/media/icofont.f6ab04ae.woff"
+  },
+  {
+    "revision": "a599107bf60cc1d46654c3a0abfceffa",
+    "url": "/static/media/knife_fire.a599107b.png"
+  },
+  {
+    "revision": "38871e9e8dcbb2c532b360aa1341fb12",
+    "url": "/static/media/logo.38871e9e.png"
+  },
+  {
+    "revision": "5901009ec215de2a3342f2679f772f07",
+    "url": "/static/media/master_chef.5901009e.png"
+  },
+  {
+    "revision": "693fd84fe071627a68a9af2381272059",
+    "url": "/static/media/next.693fd84f.svg"
+  },
+  {
+    "revision": "942aab2fbd0f63af2cd1796c39249e16",
+    "url": "/static/media/prev.942aab2f.svg"
+  },
+  {
+    "revision": "5653cb27e22ff8c466514f582f1593c2",
+    "url": "/static/media/reservation_1_bg.5653cb27.png"
+  },
+  {
+    "revision": "4f471b5d46ce755ffea73ca5599cd7e7",
+    "url": "/static/media/reservation_bg.4f471b5d.png"
+  },
+  {
+    "revision": "f35d2c0291bcf6bc7a4e00932d1b8ea0",
+    "url": "/static/media/review-banner.f35d2c02.png"
+  },
+  {
+    "revision": "984e4c7ec07a95f88de39beec6bb7258",
+    "url": "/static/media/review_01.984e4c7e.png"
+  },
+  {
+    "revision": "c6e8d94a78bd9dbf933d122243cc4a8f",
+    "url": "/static/media/review_03.c6e8d94a.png"
+  },
+  {
+    "revision": "6627ff87c12089eb93879a940b4d0a41",
+    "url": "/static/media/review_04.6627ff87.png"
+  },
+  {
+    "revision": "db4073fb4cf43e631c22a3ade3a46b8c",
+    "url": "/static/media/review_05.db4073fb.png"
+  },
+  {
+    "revision": "b36cd98a569f2f9c57144beeb4f58050",
+    "url": "/static/media/review_06.b36cd98a.png"
+  },
+  {
+    "revision": "6b05866b19b950e9318fdc2b9e50c1d8",
+    "url": "/static/media/review_07.6b05866b.png"
+  },
+  {
+    "revision": "ceb125ab251eb06c7f30919dc43f010b",
+    "url": "/static/media/testmonial_01.ceb125ab.png"
+  },
+  {
+    "revision": "16858b9e90f4f42e69fd04f7bd47e851",
+    "url": "/static/media/testmonial_02.16858b9e.png"
+  },
+  {
+    "revision": "7967f5ba53a3973a7f3fcf638f2dc351",
+    "url": "/static/media/testmonial_03.7967f5ba.png"
+  },
+  {
+    "revision": "81ebc97b9dfc4bdfccdb0a9735c74305",
+    "url": "/static/media/who_bg.81ebc97b.jpg"
+  }
+]);
